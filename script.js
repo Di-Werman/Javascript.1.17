f159c60e1df9f1968b61a9421f5f6aa00413f3a5
Di-Werman <118116167+Di-Werman@users.noreply.github.com>
2023-02-19 18:14:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// numbers.push('Not a number')
+// numbers.push('New line')
+
+// for (var i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i])
+// }
+
+// for (var i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i])
+//     }
+//  }
+
+ for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        continue 
+        }
+    console.log(numbers[i])
+    }
+
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            continue 
+            }
+        console.log(numbers[i])
+        }
+
+        for (var i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 !== 0) {
+               break 
+                }
+            console.log(numbers[i])
+            }
+
+numbers.push('String')
+    
+for (var i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] === 'string') {
+        break 
+    }
+    console.log(numbers[i])
+    }
